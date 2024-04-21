18683bf6f0855f49755057a386c4665c92c0e362
piny940 <83708535+piny940@users.noreply.github.com>
2024-04-21 16:04:36 +0900

    2024年度のカラム変更に対応する

--- src/magic.rb
+++ src/magic.rb
@@ -1,36 +1,53 @@
 require './src/web_driver'
-require './src/recruit'
 require './src/logger'
 
-CELL_INDEX = {
-  title: 1,
-  recruit_start_date: 2,
-  recruit_end_date: 3,
-  technologies: 4,
-  link: 5,
-  kind: 6,
-  company: 7,
-  area: 8,
-  reward: 9,
-  schedule1: 10,
-  schedule2: 11,
-  schedule3: 12,
-  comment: 13,
-}
+# CELL_INDEX = {
+#   title: 1,
+#   recruit_start_date: 2,
+#   recruit_end_date: 2,
+#   technologies: 3,
+#   link: 4,
+#   kind: 5,
+#   company: 6,
+#   area: 7,
+#   reward: 8,
+#   schedule1: 9,
+#   schedule2: 10,
+#   schedule3: 11,
+#   comment: 12
+# }
+CELLS = {
+  'company' => '主催',
+  'title' => 'イベント名',
+  'recruit_end_date' => '締め切り',
+  'link' => 'リンク',
+  'technologies' => '技術スタック',
+  'content' => '内容',
+  'schedule' => '実施時期/待遇',
+  'test' => '選考フロー',
+  'area' => '場所',
+  'recruit_start_date' => '募集開始',
+  'kind' => 'タグ'
+}.freeze
 module MagicSpreadsheet
+  ROW_CSS = '.notion-selectable.notion-page-block.notion-collection-item>.notion-table-view-row'.freeze
   def self.list
     logger.info('Start MagicSpreadsheet.list')
     items = []
     driver = WebDriver.driver
     driver.get(ENV.fetch('MAGIC_SPREADSHEET_URL', nil))
     sleep ENV.fetch('MAGIC_SPREADSHEET_WAIT_TIME', 5).to_i
+
+    scroll_to_bottom(driver)
+
     begin
-      rows = driver.find_elements(:css, '.notion-selectable.notion-page-block.notion-collection-item>.notion-table-view-row')
+      rows = driver.find_elements(:css, ROW_CSS)
       rows.each do |row|
         item = {}
-        CELL_INDEX.each do |k, v|
-          cell = row.find_element(:css, ".notion-table-view-cell[data-col-index='#{v}']")
-          if k == :link && cell.text != ''
+        CELLS.each_key do |k|
+          idx = CELLS.keys.index(k)
+          cell = row.find_element(:css, ".notion-table-view-cell[data-col-index='#{idx + 1}']")
+          if k == 'link' && cell.text != ''
             item[k] = cell.find_element(:css, 'a').attribute('href')
             next
           end
@@ -42,6 +59,20 @@ module MagicSpreadsheet
       driver.quit
     end
     logger.info('End MagicSpreadsheet.list')
-    items.map{ |item| Recruit.new(**item) }
+    items
+  end
+
+  def self.scroll_to_bottom(driver)
+    logger.info('Start MagicSpreadsheet.scroll_to_bottom')
+    prev_count = 0
+    loop do
+      driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+      sleep ENV.fetch('MAGIC_SPREADSHEET_WAIT_TIME', 5).to_i
+      rows = driver.find_elements(:css, ROW_CSS)
+      break if rows.size == prev_count
+
+      prev_count = rows.size
+    end
+    logger.info('End MagicSpreadsheet.scroll_to_bottom')
   end
 end
